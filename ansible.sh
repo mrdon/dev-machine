4ff24461d40eae5193a8b54c69b7607a04f7d676
Don Brown <don@reciprocitylabs.com>
2019-03-04 11:26:54 -0700

    Use symlinks, make generic vars

--- ansible.sh
+++ ansible.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
-set -x
+
 if ! which ansible-playbook > /dev/null; then 
 	sudo apt remove ansible -y
 	sudo apt-add-repository ppa:ansible/ansible
 	sudo apt update
 	sudo apt install ansible -y
+fi
+
+USER_VARS_FILE="user-vars.yml"
+if [ ! -f ${USER_VARS_FILE} ]; then
+    echo -n "Enter your display name: "
+    read display_name
+
+    echo -n "Enter your email address: "
+    read email
+
+    username=`whoami`
+    playbook_dir=`pwd`
+    cat > ${USER_VARS_FILE} << EOF1
+user: $username
+display_name: $display_name
+email: $email
+playbook_dir: $playbook_dir
+EOF1
 fi
 
 if [ -z "$1" ];
