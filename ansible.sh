32570751c9925038013a28b72fa6002cd9ba8202
Don Brown <mrdon@twdata.org>
2019-03-09 22:26:23 -0700

    Add tic80, vault

--- ansible.sh
+++ ansible.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 if ! which ansible-playbook > /dev/null; then 
-	sudo apt remove ansible -y
-	sudo apt-add-repository ppa:ansible/ansible
-	sudo apt update
-	sudo apt install ansible -y
+    sudo apt install python-pip
+    sudo pip install github3.py==1.0.0a4
+    sudo apt remove ansible -y
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt update
+    sudo apt install ansible -y
 fi
 
-USER_VARS_FILE="user-vars.yml"
+VAULT_FILE="$HOME/.dev-machine-vault"
+USER_VARS_FILE="vars/user.yml"
 if [ ! -f ${USER_VARS_FILE} ]; then
     echo -n "Enter your display name: "
     read display_name
@@ -15,8 +18,14 @@ if [ ! -f ${USER_VARS_FILE} ]; then
     echo -n "Enter your email address: "
     read email
 
+    echo -n "Enter the vault password: "
+    read vault_pwd
+
     username=`whoami`
     playbook_dir=`pwd`
+
+    echo $vault_pwd > $VAULT_FILE
+    chmod 600 $VAULT_FILE
     cat > ${USER_VARS_FILE} << EOF1
 user: $username
 display_name: $display_name
@@ -25,11 +34,17 @@ playbook_dir: $playbook_dir
 EOF1
 fi
 
+# migration
+
+if [ -f user-vars.yml ]; then
+    mv -f user-vars.yml vars/user.yml
+fi
+
 if [ -z "$1" ];
 then
     : # $1 was not given
-    ansible-playbook -i "localhost," -c local playbook.yml --ask-become-pass
+    ansible-playbook -i "localhost," -c local --vault-id $VAULT_FILE playbook.yml --ask-become-pass
 else
     : # $1 was given
-    ansible-playbook -i "localhost," -c local playbook.yml --ask-become-pass --start-at-task "$1"
+    ansible-playbook -i "localhost," -c local --vault-id $VAULT_FILE playbook.yml --ask-become-pass --start-at-task "$1"
 fi
